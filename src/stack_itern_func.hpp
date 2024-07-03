c531f3c097a73772862b425c3644c74a7ef06e07
yaya <semenov.aleksandr@phystech.edu>
2024-07-03 13:59:51 +0300

    вынес функции для отдельных типов в cpp файл, сделал библиотеку симейк

--- src/stack_itern_func.hpp
+++ src/stack_itern_func.hpp
@@ -134,32 +134,4 @@ void Stack<T>::data_print(FILE *filename, T *data)
         bin_print(filename, data, sizeof(T));
 }
 
-template<>
-void Stack<int>::data_print(FILE *filename, int *data)
-{
-    if (filename)
-        fprintf(filename, "%d\n", *data);
-}
-
-template<>
-void Stack<char>::data_print(FILE *filename, char * data)
-{
-    if (filename) 
-        fprintf(filename, "%d\n", *data);
-}
-
-template<>
-void Stack<double>::data_print(FILE *filename, double *data)
-{
-    if (filename)
-        fprintf(filename, "%.2f\n", *data);
-}
-
-template<>
-void Stack<long long int>::data_print(FILE *filename, long long int *data)
-{
-    if (filename)
-        fprintf(filename, "%lld\n", *data);
-}
-
 #endif
